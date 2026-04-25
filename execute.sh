cb30bf0c09ee2e8097e0973b18cafd319c8ee38e
sennasama <oyasumi.senko@gmail.com>
2026-04-25 23:31:08 +0700

    chore: adjust 23:31:08

--- execute.sh
+++ execute.sh
@@ -1,33 +1,28 @@
 #!/bin/bash
 
-# Hapus token dari script, baca dari env
-
 git remote set-url origin https://sennasama:${GH_TOKEN}@github.com/sennasama/farmAct.git
 
 git config user.email "oyasumi.senko@gmail.com"
-
 git config user.name "sennasama"
 
 WORDS=("update" "fix" "patch" "sync" "refresh" "tweak" "adjust" "clean")
+FILES=("data.txt" "log.txt" "notes.txt" "cache.txt" "temp.txt")
 
-while true; do
+for i in $(seq 1 365); do
+  HOUR=$((RANDOM % 12 + 8))
+  MIN=$((RANDOM % 60))
+  SEC=$((RANDOM % 60))
+  DATE=$(date -d "$i days ago" +"%Y-%m-%dT${HOUR}:${MIN}:${SEC}")
 
-  FILENAME="file_$((RANDOM % 10)).txt"
-
-  echo "$(date) - $RANDOM" >> $FILENAME
+  FILENAME=${FILES[$((RANDOM % ${#FILES[@]}))]}
+  echo "$DATE - $RANDOM" >> $FILENAME
 
   WORD=${WORDS[$((RANDOM % ${#WORDS[@]}))]}
 
-  git add .
+  GIT_AUTHOR_DATE="$DATE" GIT_COMMITTER_DATE="$DATE" \
+    git commit -am "chore: $WORD $i" --allow-empty
 
-  git commit -m "chore: $WORD $(date +%H:%M:%S)"
-
-  git push origin main
-
-  SLEEP=$((RANDOM % 1))
-
-  echo "Sleeping ${SLEEP}s..."
-
-  sleep $SLEEP
-
+  echo "Done: $DATE"
 done
+
+git push origin main
